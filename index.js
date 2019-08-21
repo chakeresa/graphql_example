ce58717d7f6a493b3b1f0600467dd8bc557ad7a6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-21 11:02:58 -0600

    Add schema, root, pets, getPet, getPets

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const express = express = require("express");
+const express = require("express");
 const express_graphql = express_graphql = require("express-graphql");
 const { buildSchema } = { buildSchema } = require("graphql");
 const app = express();
+
+const schema = buildSchema(`
+  type Query {
+    pet(id: Int!): Pet
+    pets(animalType: String): [Pet]
+  },
+  type Pet {
+    id: Int
+    name: String
+    animalType: String
+  }
+`)
+
+let pets = [
+  { id: 1, name: "Zoe", animalType: "dog" },
+  { id: 2, name: "Kima", animalType: "dog" },
+  { id: 3, name: "Penny", animalType: "cat" }
+]
+
+function getPet(id) {
+  return pets.find(function(pet) {
+    return pet.id === id
+  })
+}
+
+function getPets(animalType) {
+  // TODO: filter down to a particular animalType
+  return pets
+}
+
+const root = {
+  pet: getPet,
+  pets: getPets
+}
 
 app.use('/graphql', express_graphql({
   schema: schema,
